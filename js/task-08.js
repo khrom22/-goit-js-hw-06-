d34f50a591c0c333f7adc20f21c36363b54fc027
Roman Khomyn <107980144+khrom22@users.noreply.github.com>
2023-11-09 01:13:58 +0200

    виконано 1-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', handleSubmit)
+
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const { email, password } = event.currentTarget.elements
+    const formObj = {
+        email: email.value,
+        password: password.value,
+    }
+
+    if (formObj.email === '' || formObj.password === '') {
+        alert('Всі поля повинні бути заповнені')
+    }
+
+    console.log(formObj)
+    event.currentTarget.reset()
+}
+
+
